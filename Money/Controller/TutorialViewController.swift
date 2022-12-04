f390f4e79af10b2178b846a9d615b9de0559ab75
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-12-03 19:38:12 -0500

    tests fixes

--- Money/Controller/TutorialViewController.swift
+++ Money/Controller/TutorialViewController.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 class TutorialViewController: UIViewController {
+
+    // MARK: Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if CommandLine.arguments.contains("--moneyScreenshots") {
+            // We are in testing mode, make arrangements if needed
+            UIView.setAnimationsEnabled(false)
+        }
+
+    }
+
+
+    // MARK: Helpers
+
     @IBAction func doneTapped(_ sender: Any) {
         dismiss(animated: true)
     }
