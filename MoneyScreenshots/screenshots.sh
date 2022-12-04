05937726b36889e800d0a2e9659a52a543069cd0
Daniel Springer <19246409+DaniSpringer@users.noreply.github.com>
2022-12-03 19:54:41 -0500

    prettify shell script

--- MoneyScreenshots/screenshots.sh
+++ MoneyScreenshots/screenshots.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
 #  screenshots.sh
 #  Money
 #
 #  Created by dani on 11/17/22.
-#  
+#
 
 # The Xcode project to create screenshots for
 projectName="./Money.xcodeproj"
@@ -12,56 +12,51 @@ projectName="./Money.xcodeproj"
 # The scheme to run tests for
 schemeName="Money"
 
-
 # All the simulators we want to screenshot
 # Copy/Paste new names from Xcode's
 # "Devices and Simulators" window
 # or from `xcrun simctl list`.
 
 simulators=(
-"iPhone 14 Pro Max"
-"iPhone 13 Pro Max"
-"iPhone 8 plus"
-"iPad Pro (12.9-inch) (6th generation)"
+    "iPhone 14 Pro Max"
+    "iPhone 13 Pro Max"
+    "iPhone 8 plus"
+    "iPad Pro (12.9-inch) (6th generation)"
 )
 
 # All the languages we want to screenshot (ISO 3166-1 codes)
 languages=(
-"en"
+    "en"
 )
 
 # All the appearances we want to screenshot
 # (options are "light" and "dark")
 appearances=(
-"light"
+    "light"
 )
 
 # Save final screenshots into this folder (it will be created)
 targetFolder="/Users/dani/Desktop/MoneyScreenshots"
 rm -rf /Users/dani/Desktop/MoneyScreenshots
 
+for simulator in "${simulators[@]}"; do
+    for language in "${languages[@]}"; do
+        for appearance in "${appearances[@]}"; do
+            rm -rf /tmp/MoneyDerivedData/Logs/Test
+            echo "📲 Building and Running for $simulator in $language"
 
-for simulator in "${simulators[@]}"
-do
-for language in "${languages[@]}"
-do
-for appearance in "${appearances[@]}"
-do
-rm -rf /tmp/MoneyDerivedData/Logs/Test
-echo "📲 Building and Running for $simulator in $language"
+            # Boot up the new simulator and set it to
+            # the correct appearance
+            xcrun simctl boot "$simulator"
+            xcrun simctl ui "$simulator" appearance $appearance
 
-# Boot up the new simulator and set it to
-# the correct appearance
-xcrun simctl boot "$simulator"
-xcrun simctl ui "$simulator" appearance $appearance
+            # Build and Test
+            xcodebuild -testLanguage $language -scheme $schemeName -project $projectName -derivedDataPath '/tmp/MoneyDerivedData/' -destination "platform=iOS Simulator,name=$simulator" build test
+            echo "Collecting Results..."
+            mkdir -p "$targetFolder/$simulator/"
+            find /tmp/MoneyDerivedData/Logs/Test -maxdepth 1 -type d -exec xcparse screenshots {} "$targetFolder/$simulator/" \;
+        done
+    done
 
-# Build and Test
-xcodebuild -testLanguage $language -scheme $schemeName -project $projectName -derivedDataPath '/tmp/MoneyDerivedData/' -destination "platform=iOS Simulator,name=$simulator" build test
-echo "Collecting Results..."
-mkdir -p "$targetFolder/$simulator/"
-find /tmp/MoneyDerivedData/Logs/Test -maxdepth 1 -type d -exec xcparse screenshots {} "$targetFolder/$simulator/" \;
-done
-done
-
-echo "✅ Done"
+    echo "✅ Done"
 done
